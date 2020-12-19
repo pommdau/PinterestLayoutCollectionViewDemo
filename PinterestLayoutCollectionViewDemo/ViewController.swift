d517dfaae2e96e19db133e538523a1df0bd32e86
hikeuchi <hikeuchi@morisawa.co.jp>
2020-12-19 23:35:47 +0900

    initial commit

---
+++ PinterestLayoutCollectionViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PinterestLayoutCollectionViewDemo
+//
+//  Created by HIROKI IKEUCHI on 2020/12/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
